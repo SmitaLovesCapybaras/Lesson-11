df7fbbf304cbc92135c5ae8c69b858d624dd974b
SmitaLovesCapybaras <Baggiapari@gmail.com>
2026-01-15 19:42:50 -0500

    Add files via upload

---
+++ Extra question.py
@@ -0,0 +1,13 @@
+# n=int(input("Enter a number:"))
+# i=1
+# while i<=n:
+#     print(i)
+#     i=i+1
+
+
+n=int(input("Enter a number between 1 and 20:"))
+i=1
+while i<=n:
+    if i%2==0:
+        print(i)
+    i=i+1
